4c1bf6a93288ed958834bb3690a0111b94f1c0a2
HoussamAW <houssamabdoulwahab@gmail.com>
2025-11-11 14:04:49 +0100

    Update function to search more film

--- MovieDbLike/Model.swift
+++ MovieDbLike/Model.swift
@@ -23,15 +23,22 @@ struct MoviesResponse: Codable {
     let results: [Movie]
 }
 
-func performAPICall(language:String) async throws -> [Movie] {
+func performAPICall(language: String) async throws -> [Movie] {
+    var allMovies: [Movie] = []
+
+    for page in 1...200 {
+        let urlString = "https://api.themoviedb.org/3/movie/popular?api_key=\(APIKey)&language=\(language)&page=\(page)"
+        guard let url = URL(string: urlString) else { continue }
+
+        let (data, _) = try await URLSession.shared.data(from: url)
+
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+        let decoded = try decoder.decode(MoviesResponse.self, from: data)
+        allMovies.append(contentsOf: decoded.results)
+    }
     
-    let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=\(APIKey)&language=\(language)&page=1")!
-    let (data, _) = try await URLSession.shared.data(from: url)
-    
-    let decoder = JSONDecoder()
-    decoder.keyDecodingStrategy = .convertFromSnakeCase // la ligne magique qui a regler tout mes souscis : converti le underscore name du JSON en camel case 
-    
-    let response = try decoder.decode(MoviesResponse.self, from: data)
-    return response.results
+    return allMovies
 }
 
